13ab16ef8712f838917c0d5b49f4d695229fdc32
ChenhuiGu <guchh@pbcsf.tsinghua.edu.cn>
2019-03-16 15:11:10 +0800

    change the baidu api

--- baidu_config.py
+++ baidu_config.py
@@ -5,15 +5,12 @@ from aip import AipNlp
 # SECRET_KEY = '7ThNTz1lDjqzX6VxjZh0YieAMZ1As6TT'
 
 #pbcsfnlp01-Wjbb12345
-APP_ID = '15709863'
-API_KEY = 'DuawmmStVg9UuBhvtRc4oieP'
-SECRET_KEY = 'eo8tyyErCZlP8vzcz7bCRiEuZWuPlS3w'
+APP_ID = '15771747'
+API_KEY = 'bTKfcVBinZfhfFqL6X6zoQEh'
+SECRET_KEY = 'y5yoBOyBkLa0pUwn3YQpDls0tRM8epH0'
 
 bd_client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
 
 urltoken ='https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials'\
           +'&client_id=' + API_KEY\
           +'&client_secret=' + SECRET_KEY
-
-APP_ID_MAP = '15337368'
-API_KEY_MAP = 'yYVDtPm57GllrGCyzFOo1xOuiIOlpNLt'
